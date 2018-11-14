f494860a3557b3825db23690cfd8d811848957c3
zzy1661 <1556597051@qq.com>
2018-11-15 02:05:51 +0800

    add entity目录

---
+++ entity/User.js
@@ -0,0 +1,29 @@
+class User {
+
+    constructor({id=null, name='') {
+        this.id = id;
+        this.name = name;
+    }
+
+    getTasks() {
+
+    }
+
+    getTaskById() {
+
+    }
+
+    createTask(task) {
+
+    }
+
+    deleteTask(task) {
+
+    }
+
+    updateTask(task) {
+
+    }
+
+}
+module.exports = User;
